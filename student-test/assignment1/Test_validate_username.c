af0baf405ad4470bfbd003d3e1fe9806817c5aa0
apropos0 <Aproposofnothing1>
2024-05-11 13:06:33 -0700

    fix test

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -19,5 +19,5 @@ void test_validate_my_username()
      * config file and my_username() functions are setup properly
      */
     
-    TEST_ASSERT_TRUE_MESSAGE(my_username() == malloc_username_from_conf_file(),"AESD students, please fix me!");
+	TEST_ASSERT_EQUAL_STRING_MESSAGE(my_username(), malloc_username_from_conf_file(), "apropos0");
 }
